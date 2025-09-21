bf6f271b425cfeb0726be3bd2b82fbe18d7887ac
NguyenVanTong <22662921.tong@student.iuh.edu.vn>
2025-09-21 21:56:21 +0700

    add UTLab05

---
+++ program/HuyChuoi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHKTPM18ATT_Tong_Phuc_Long_TienPhat.program
+{
+    public class Program
+    {
+        public static string HuyChuoi(string s, int n, int p)
+        {
+            if (string.IsNullOrEmpty(s)) return s;
+            if (p < 0 || n < 0) return s;
+            if (p >= s.Length) return s;
+
+            if (n > s.Length - p)
+            {
+                return s.Substring(0, p);
+            }
+
+            string left = s.Substring(0, p);
+            string right = s.Substring(p + n);
+            return left + right;
+        }
+    }
+}
---
+++ test/UTLab5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DHKTPM18ATT_Tong_Phuc_Long_TienPhat.program;
+
+namespace DHKTPM18ATT_Tong_Phuc_Long_TienPhat.test
+{
+    [TestClass]
+    public class UTLab5
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\data\\UTLab05.csv",
+            "UTLab05#csv",
+            DataAccessMethod.Sequential),
+        DeploymentItem("data\\UTLab05.csv"),
+        TestMethod]
+        public void TestHuyChuoi()
+        {
+            string input = TestContext.DataRow["s"].ToString();
+            int n = Convert.ToInt32(TestContext.DataRow["n"]);
+            int p = Convert.ToInt32(TestContext.DataRow["p"]);
+            string expected = TestContext.DataRow["ExpectedResult"].ToString();
+
+            string actual = Program.HuyChuoi(input, n, p);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
