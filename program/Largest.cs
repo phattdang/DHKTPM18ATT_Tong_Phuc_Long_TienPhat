90641caa0b3ef85ef77516300df73bdf20889235
phatdang <phatdang19052004@gmail.com>
2025-09-23 14:27:35 +0700

    Hoàn thiện mã kiểm thử cho UTLab07 và UTLab08

---
+++ program/Largest.cs
@@ -0,0 +1,24 @@
+﻿namespace DHKTPM18ATT_Tong_Phuc_Long_TienPhat.program
+{
+    internal class Largest
+    {
+        public static int FindLargest(int[] a)
+        {
+            if (a == null || a.Length == 0)
+            {
+                return int.MaxValue; // 2147483647
+            }
+
+            int max = a[0];
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
--- test/UTLab5.cs
+++ test/UTLab5.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DHKTPM18ATT_Tong_Phuc_Long_TienPhat.program;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using DHKTPM18ATT_Tong_Phuc_Long_TienPhat.program;
+using System;
 
 namespace DHKTPM18ATT_Tong_Phuc_Long_TienPhat.test
 {
